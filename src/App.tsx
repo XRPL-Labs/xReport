756600bc9faba899d137b6e400a0cf9d0140630e
Kevin Koobs <k.koobs@outlook.com>
2023-02-21 15:53:22 +0100

    First setup and added xApp Stylesheet

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function handleSubmit() {
+
+}
+
+function App() {
+  return (
+    <div className="App flex">
+      <h1 className="font-bold text-3xl">You want to revoke access for Xumm Pro Beta</h1>
+        
+    </div>
+  )
+}
+
+export default App
